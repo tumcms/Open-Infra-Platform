fdb6f29d521c2375bdaa7fd49b31c34f91eef004
Julian Amann <julian.amann@tum.de>
2018-02-09 15:05:07 +0100

    Removed old python linked data test code.

--- Infrastructure/src/OpenInfraPlatform/Infrastructure/Alignment/AlignmentModel.cpp
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/Alignment/AlignmentModel.cpp
@@ -31,117 +31,117 @@ AlignmentModel::~AlignmentModel()
 
 buw::AxisAlignedBoundingBox3d AlignmentModel::getExtends() const
 {
-	// we run along along all alignments an determine the corresponding position
-	// this my seem stupid at first glance, but in this way we have not to introduce
-	// additional methods in the 2D alignment element class.
-	if (alignments_.size() == 0)
-	{
-		return buw::AxisAlignedBoundingBox3d();
-	}
+    // we run along along all alignments an determine the corresponding position
+    // this my seem stupid at first glance, but in this way we have not to introduce
+    // additional methods in the 2D alignment element class.
+    if (alignments_.size() == 0)
+    {
+        return buw::AxisAlignedBoundingBox3d();
+    }
 
-	buw::Vector3d min_ = alignments_[0]->getPosition(0);
-	buw::Vector3d max_ = alignments_[0]->getPosition(0);
+    buw::Vector3d min_ = alignments_[0]->getPosition(0);
+    buw::Vector3d max_ = alignments_[0]->getPosition(0);
 
-	for (int i = 0; i < alignments_.size(); i++)
-	{
-		for (double s = alignments_[i]->getStartStation(); s < alignments_[i]->getEndStation(); s += 1.0)
-		{
-			auto p = alignments_[i]->getPosition(s);
-			min_ = buw::minimizedVector(min_, p);
-			max_ = buw::maximizedVector(max_, p);
-		}
-	}
+    for (int i = 0; i < alignments_.size(); i++)
+    {
+        for (double s = alignments_[i]->getStartStation(); s < alignments_[i]->getEndStation(); s += 1.0)
+        {
+            auto p = alignments_[i]->getPosition(s);
+            min_ = buw::minimizedVector(min_, p);
+            max_ = buw::maximizedVector(max_, p);
+        }
+    }
 
-	return buw::AxisAlignedBoundingBox3d(min_, max_);
+    return buw::AxisAlignedBoundingBox3d(min_, max_);
 }
 
 void AlignmentModel::deleteAlignment(buw::ReferenceCounted<buw::IAlignment3D> alignment)
 {
-	auto iterator = std::find(alignments_.begin(), alignments_.end(), alignment);
+    auto iterator = std::find(alignments_.begin(), alignments_.end(), alignment);
 
-	BLUE_ASSERT(iterator != alignments_.end(), "Invalid alignment");
+    BLUE_ASSERT(iterator != alignments_.end(), "Invalid alignment");
 
-	if (iterator != alignments_.end())
-	{
-		alignments_.erase(iterator);
-	}
-	else
-	{
-		throw std::runtime_error("Deltion of alignment failed.");
-	}
+    if (iterator != alignments_.end())
+    {
+        alignments_.erase(iterator);
+    }
+    else
+    {
+        throw std::runtime_error("Deltion of alignment failed.");
+    }
 }
 
 void AlignmentModel::addAlignment(buw::ReferenceCounted<buw::IAlignment3D> alignment)
 {
-	alignments_.push_back(alignment);
+    alignments_.push_back(alignment);
 }
 
 std::vector<buw::ReferenceCounted<buw::IAlignment3D>> AlignmentModel::getAlignments()
 {
-	return alignments_;
+    return alignments_;
 }
 
 buw::ReferenceCounted<buw::IAlignment3D> AlignmentModel::getAlignment(const int index) const
 {
-	return alignments_[index];
+    return alignments_[index];
 }
 
 buw::ReferenceCounted<buw::IAlignment3D> AlignmentModel::getAlignmentById(const int id) const {
-	if (alignments_.size() == 0)
-	{
-		return nullptr;
-	}
+    if (alignments_.size() == 0)
+    {
+        return nullptr;
+    }
 
-	for (int i = 0; i < alignments_.size(); ++i) {
-		if (alignments_[i]->getType() == buw::e3DAlignmentType::e2DBased) {
-			buw::ReferenceCounted<buw::Alignment2DBased3D> a = std::static_pointer_cast<buw::Alignment2DBased3D>(alignments_[i]);
-			if (a->getId() == id)
-				return a;
-		}
+    for (int i = 0; i < alignments_.size(); ++i) {
+        if (alignments_[i]->getType() == buw::e3DAlignmentType::e2DBased) {
+            buw::ReferenceCounted<buw::Alignment2DBased3D> a = std::static_pointer_cast<buw::Alignment2DBased3D>(alignments_[i]);
+            if (a->getId() == id)
+                return a;
+        }
 
-	}
+    }
 
-	return nullptr;
+    return nullptr;
 }
 
 int AlignmentModel::getAlignmentCount() const
 {
-	return static_cast<int>(alignments_.size());
+    return static_cast<int>(alignments_.size());
 }
 
 buw::AxisAlignedBoundingBox3d buw::AlignmentModel::getVerticalAlignmentExtends()
 {
-	buw::AxisAlignedBoundingBox3d b;
+    buw::AxisAlignedBoundingBox3d b;
 
-	bool firstFound = true;
+    bool firstFound = true;
 
-	for (int i = 0; i < alignments_.size(); i++)
-	{
-		if (alignments_[i]->getType() == buw::e3DAlignmentType::e2DBased)
-		{
-			buw::ReferenceCounted<buw::Alignment2DBased3D> a = std::static_pointer_cast<buw::Alignment2DBased3D>(alignments_[i]);
+    for (int i = 0; i < alignments_.size(); i++)
+    {
+        if (alignments_[i]->getType() == buw::e3DAlignmentType::e2DBased)
+        {
+            buw::ReferenceCounted<buw::Alignment2DBased3D> a = std::static_pointer_cast<buw::Alignment2DBased3D>(alignments_[i]);
 
-			if (firstFound)
-			{
-				firstFound = false;
-				b = a->getVerticalAlignmentExtends();
-			}
-			else
-			{
-				auto u = buw::minimizedVector( b.getMinimum(), a->getVerticalAlignmentExtends().getMinimum() );
-				auto v = buw::minimizedVector( b.getMaximum(), a->getVerticalAlignmentExtends().getMaximum() );
-				b.setMinimum(u);
-				b.setMaximum(v);
-			}
-		}
-	}
+            if (firstFound)
+            {
+                firstFound = false;
+                b = a->getVerticalAlignmentExtends();
+            }
+            else
+            {
+                auto u = buw::minimizedVector( b.getMinimum(), a->getVerticalAlignmentExtends().getMinimum() );
+                auto v = buw::minimizedVector( b.getMaximum(), a->getVerticalAlignmentExtends().getMaximum() );
+                b.setMinimum(u);
+                b.setMaximum(v);
+            }
+        }
+    }
 
-	return b;
+    return b;
 }
 
 buw::AlignmentModel* buw::AlignmentModel::createFlatCopy(const AlignmentModel& src)
 {
-	return new AlignmentModel(src);
+    return new AlignmentModel(src);
 }
 
 OIP_NAMESPACE_OPENINFRAPLATFORM_INFRASTRUCTURE_END
--- Infrastructure/src/OpenInfraPlatform/Infrastructure/Export/ExportOkstraOWL.cpp
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/Export/ExportOkstraOWL.cpp
@@ -25,188 +25,209 @@ OpenInfraPlatform::Infrastructure::ExportOkstraOWL::ExportOkstraOWL(buw::Referen
                                                                     buw::ReferenceCounted<buw::DigitalElevationModel> dem,
                                                                     const std::string& filename)
     : Export(am, dem, filename) {
-	if(false) // foaf test
-	{
-		FILE* outfile = fopen(filename.c_str(), "w");
+    if(false) // foaf test
+    {
+        FILE* outfile = fopen(filename.c_str(), "w");
 
-		raptor_world *world = raptor_new_world();
+        raptor_world *world = raptor_new_world();
 
-		raptor_serializer* rdf_serializer = raptor_new_serializer(world, "rdfxml-abbrev");
-		raptor_serializer_start_to_file_handle(rdf_serializer, nullptr, outfile);
+        raptor_serializer* rdf_serializer = raptor_new_serializer(world, "rdfxml-abbrev");
+        raptor_serializer_start_to_file_handle(rdf_serializer, nullptr, outfile);
 
-		const unsigned char* foaf_prefix = (const unsigned char*)"foaf";
-		raptor_uri* foaf_uri = raptor_new_uri(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/");
-		raptor_serializer_set_namespace(rdf_serializer, foaf_uri, foaf_prefix);
+        const unsigned char* foaf_prefix = (const unsigned char*)"foaf";
+        raptor_uri* foaf_uri = raptor_new_uri(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/");
+        raptor_serializer_set_namespace(rdf_serializer, foaf_uri, foaf_prefix);
 
-		const unsigned char* rdf_prefix = (const unsigned char*)"rdf";
-		raptor_uri* rdf_uri = raptor_new_uri(world, (const unsigned char*)"http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-		raptor_serializer_set_namespace(rdf_serializer, rdf_uri, rdf_prefix);
+        const unsigned char* rdf_prefix = (const unsigned char*)"rdf";
+        raptor_uri* rdf_uri = raptor_new_uri(world, (const unsigned char*)"http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        raptor_serializer_set_namespace(rdf_serializer, rdf_uri, rdf_prefix);
 
-		{
-			raptor_statement* triple = NULL; triple = raptor_new_statement(world);
+        {
+            raptor_statement* triple = NULL; triple = raptor_new_statement(world);
 
-			triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/name");
-			triple->object = raptor_new_term_from_literal(world, (unsigned char*)"Jimmy Wales", NULL, NULL);
+            triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/name");
+            triple->object = raptor_new_term_from_literal(world, (unsigned char*)"Jimmy Wales", NULL, NULL);
 
-			raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
-		}
+            raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
+        }
 
-		{
-			raptor_statement* triple = NULL; triple = raptor_new_statement(world);
+        {
+            raptor_statement* triple = NULL; triple = raptor_new_statement(world);
 
-			triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/mbox");
-			triple->object = raptor_new_term_from_uri_string(world, (unsigned char*)"mailto:jwales@bomis.com");
+            triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://xmlns.com/foaf/0.1/mbox");
+            triple->object = raptor_new_term_from_uri_string(world, (unsigned char*)"mailto:jwales@bomis.com");
 
-			raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
-		}
+            raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
+        }
 
-		{
-			raptor_statement* triple = NULL; triple = raptor_new_statement(world);
+        {
+            raptor_statement* triple = NULL; triple = raptor_new_statement(world);
 
-			triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
-			triple->object = raptor_new_term_from_uri_string(world, (unsigned char*)"http://xmlns.com/foaf/0.1/Person");
+            triple->subject = raptor_new_term_from_blank(world, (const unsigned char*)"b1");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+            triple->object = raptor_new_term_from_uri_string(world, (unsigned char*)"http://xmlns.com/foaf/0.1/Person");
 
-			raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
-		}
+            raptor_serializer_serialize_statement(rdf_serializer, triple); raptor_free_statement(triple);
+        }
 
-		raptor_serializer_serialize_end(rdf_serializer);
-		raptor_free_serializer(rdf_serializer);
-	
-		raptor_free_world(world);
+        raptor_serializer_serialize_end(rdf_serializer);
+        raptor_free_serializer(rdf_serializer);
+    
+        raptor_free_world(world);
 
-		fclose(outfile);
+        fclose(outfile);
 
-		return;
-	}
+        return;
+    }
 
-	if (false) {
-		FILE* outfile = fopen(filename.c_str(), "w");
+    if (false) {
+        FILE* outfile = fopen(filename.c_str(), "w");
 
-		raptor_world* world = raptor_new_world();
+        raptor_world* world = raptor_new_world();
 
-		raptor_serializer* rdf_serializer;
-		rdf_serializer = raptor_new_serializer(world, "turtle");
-		raptor_serializer_start_to_file_handle(rdf_serializer, nullptr, outfile);
+        raptor_serializer* rdf_serializer;
+        rdf_serializer = raptor_new_serializer(world, "turtle");
+        raptor_serializer_start_to_file_handle(rdf_serializer, nullptr, outfile);
 
-		const unsigned char* prefix = (const unsigned char*)"okstra";
-		raptor_uri* uri = raptor_new_uri(world, (const unsigned char*)"Data/okstra#");
-		raptor_serializer_set_namespace(rdf_serializer, uri, prefix);
+        const unsigned char* prefix = (const unsigned char*)"okstra";
+        raptor_uri* uri = raptor_new_uri(world, (const unsigned char*)"Data/okstra#");
+        raptor_serializer_set_namespace(rdf_serializer, uri, prefix);
 
-		for (int i = 0; i < am->getAlignmentCount(); i++) {
-			buw::String id = am->getAlignments()[i]->getName();
+        for (int i = 0; i < am->getAlignmentCount(); i++) {
+            buw::String id = am->getAlignments()[i]->getName();
 
-			raptor_statement* triple = nullptr;
+            raptor_statement* triple = nullptr;
 
-			triple = raptor_new_statement(world);
-			triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#hat_Achselement_Achse");
-			triple->object = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Achselement_List");
-			raptor_serializer_serialize_statement(rdf_serializer, triple);
-			raptor_free_statement(triple);
+            triple = raptor_new_statement(world);
+            triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#hat_Achselement_Achse");
+            triple->object = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Achselement_List");
+            raptor_serializer_serialize_statement(rdf_serializer, triple);
+            raptor_free_statement(triple);
 
-			triple = raptor_new_statement(world);
-			triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Kennung");
-			triple->object = raptor_new_term_from_literal(world, (unsigned char*)id.toCString(), nullptr, nullptr);
-			raptor_serializer_serialize_statement(rdf_serializer, triple);
-			raptor_free_statement(triple);
+            triple = raptor_new_statement(world);
+            triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Kennung");
+            triple->object = raptor_new_term_from_literal(world, (unsigned char*)id.toCString(), nullptr, nullptr);
+            raptor_serializer_serialize_statement(rdf_serializer, triple);
+            raptor_free_statement(triple);
 
-			triple = raptor_new_statement(world);
-			triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Langtext");
-			triple->object = raptor_new_term_from_literal(world, (const unsigned char*)"Hauptachse 1", nullptr, nullptr);
-			raptor_serializer_serialize_statement(rdf_serializer, triple);
-			raptor_free_statement(triple);
-		}
+            triple = raptor_new_statement(world);
+            triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Hauptachse");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Langtext");
+            triple->object = raptor_new_term_from_literal(world, (const unsigned char*)"Hauptachse 1", nullptr, nullptr);
+            raptor_serializer_serialize_statement(rdf_serializer, triple);
+            raptor_free_statement(triple);
+        }
 
-		if (dem->hasSurfaces()) {
-			raptor_statement* triple = nullptr;
+        if (dem->hasSurfaces()) {
+            raptor_statement* triple = nullptr;
 
-			triple = raptor_new_statement(world);
-			triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#DGM");
-			triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Bezeichnung");
-			triple->object = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Geländemodell");
-			raptor_serializer_serialize_statement(rdf_serializer, triple);
-			raptor_free_statement(triple);
+            triple = raptor_new_statement(world);
+            triple->subject = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#DGM");
+            triple->predicate = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Bezeichnung");
+            triple->object = raptor_new_term_from_uri_string(world, (const unsigned char*)"Data/okstra#Geländemodell");
+            raptor_serializer_serialize_statement(rdf_serializer, triple);
+            raptor_free_statement(triple);
 
-			for (int i = 0; i < dem->getSurfaceCount(); i++) {
-			}
-		}
+            for (int i = 0; i < dem->getSurfaceCount(); i++) {
+            }
+        }
 
-		raptor_serializer_serialize_end(rdf_serializer);
+        raptor_serializer_serialize_end(rdf_serializer);
 
-		raptor_free_serializer(rdf_serializer);
-		raptor_free_world(world);
+        raptor_free_serializer(rdf_serializer);
+        raptor_free_world(world);
 
-		fclose(outfile);
+        fclose(outfile);
 
-		return;
-	}
-
-
-	std::ofstream out(filename.c_str());
-
-	out << std::setprecision(9) << std::showpoint << std::fixed;
-
-	out << "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ." << std::endl;
-	out << "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." << std::endl;
-	out << "@prefix okstra: <http://okstraowl.org/def/2017/okstraowl#> ." << std::endl;
-	out << std::endl;
-
-	for (int i = 0; i < am->getAlignmentCount(); i++) {
-		buw::String name = am->getAlignments()[i]->getName();
-
-		out << "<#" << name.toCString() << ">" << std::endl;
-		out << "\t" << "a okstra:Trasse ;" << std::endl;
-		out << "\t" << "okstra:Bezeichnung \"" << name.toCString() << "\"";
-		out << " ." << std::endl << std::endl;
-	}
-
-	for (int i = 0; i < dem->getSurfaceCount(); i++) {
-		buw::String name = dem->getSurface(i)->getName();
-
-		out << "<#" << name.toCString() << ">" << std::endl;
-		out << "\t" << "a okstra:DGM ;" << std::endl;
-		out << "\t" << "okstra:Bezeichnung \"" << name.toCString() << "\"" << " ;" << std::endl;
-
-		out << "\t" << "okstra:hat_Dreiecke [" << std::endl;
+        return;
+    }
 
 
-		buw::ReferenceCounted<buw::Surface> surface = dem->getSurface(i);
+    std::ofstream out(filename.c_str());
 
-		const std::vector<buw::Vector3d>& points = surface->getPoints();
-		
-		const std::vector<buw::Vector3i>& faces = surface->getTriangeFaces();
-		for (size_t i = 0; i < faces.size(); ++i) { // t buw::Vector3i& face : faces) {
-			const buw::Vector3i& face = faces[i];
+    out << std::setprecision(9) << std::showpoint << std::fixed;
 
-			out << "\t\t" << "okstra:Dreieck [" << std::endl;
+    out << "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ." << std::endl;
+    out << "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." << std::endl;
+    out << "@prefix okstra: <http://okstraowl.org/def/2017/okstraowl#> ." << std::endl;
+    out << std::endl;
 
-			const buw::Vector3d& p0 = points[face[0]];
-			const buw::Vector3d& p1 = points[face[1]];
-			const buw::Vector3d& p2 = points[face[2]];
+    for (int i = 0; i < am->getAlignmentCount(); i++) {
+        buw::String name = am->getAlignments()[i]->getName();
 
-			out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p0.x() << " " << p0.y() << " " << p0.z() << ")\" ;" << std::endl;
-			out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p1.x() << " " << p1.y() << " " << p1.z() << ")\" ;" << std::endl;
-			out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p2.x() << " " << p2.y() << " " << p2.z() << ")\"" << std::endl;
+        out << "<#" << name.toCString() << ">" << std::endl;
+        out << "\t" << "a okstra:Trasse ;" << std::endl;
+        out << "\t" << "okstra:Bezeichnung \"" << name.toCString() << "\"" << std::endl;
 
-			out << "\t\t" << "]";
 
-			if (i != faces.size()-1) {
-				out << " ;";
-			}
+        out << "\t" << "okstra:hat_Achse [" << std::endl;
+        
+        buw::ReferenceCounted<buw::IAlignment3D> alignment = alignmentModel_->getAlignment(i);
+        buw::ReferenceCounted<buw::Alignment2DBased3D> alignment2D = std::static_pointer_cast<buw::Alignment2DBased3D>(alignment);
+        buw::ReferenceCounted<buw::HorizontalAlignment2D> horizontalAlignment = alignment2D->getHorizontalAlignment();
+        buw::ReferenceCounted<buw::VerticalAlignment2D> verticalAlignment = alignment2D->getVerticalAlignment();
 
-			out << std::endl;
-		}
+        for (int i = 0; i < horizontalAlignment->getAlignmentElementCount(); i++) {
+        }
+        out << "\t\t" << "okstra:Achse [" << std::endl;
 
-		
-		out <<  "\t" << "] ." << std::endl << std::endl;
-	}
 
-	out.close();
+
+
+        out << "\t\t" << "] ;" << std::endl;
+
+
+        out << "\t" << "] ;" << std::endl;
+
+        out << " ." << std::endl << std::endl;
+    }
+
+    for (int i = 0; i < dem->getSurfaceCount(); i++) {
+        buw::String name = dem->getSurface(i)->getName();
+
+        out << "<#" << name.toCString() << ">" << std::endl;
+        out << "\t" << "a okstra:DGM ;" << std::endl;
+        out << "\t" << "okstra:Bezeichnung \"" << name.toCString() << "\"" << " ;" << std::endl;
+
+        out << "\t" << "okstra:hat_Dreiecke [" << std::endl;
+
+
+        buw::ReferenceCounted<buw::Surface> surface = dem->getSurface(i);
+
+        const std::vector<buw::Vector3d>& points = surface->getPoints();
+        
+        const std::vector<buw::Vector3i>& faces = surface->getTriangeFaces();
+        for (size_t i = 0; i < faces.size(); ++i) { // t buw::Vector3i& face : faces) {
+            const buw::Vector3i& face = faces[i];
+
+            out << "\t\t" << "okstra:Dreieck [" << std::endl;
+
+            const buw::Vector3d& p0 = points[face[0]];
+            const buw::Vector3d& p1 = points[face[1]];
+            const buw::Vector3d& p2 = points[face[2]];
+
+            out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p0.x() << " " << p0.y() << " " << p0.z() << ")\" ;" << std::endl;
+            out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p1.x() << " " << p1.y() << " " << p1.z() << ")\" ;" << std::endl;
+            out << "\t\t\t" << "okstra:allgemeines_Punktobjekt " << "\"Point(" << p2.x() << " " << p2.y() << " " << p2.z() << ")\"" << std::endl;
+
+            out << "\t\t" << "]";
+
+            if (i != faces.size()-1) {
+                out << " ;";
+            }
+
+            out << std::endl;
+        }
+
+        
+        out <<  "\t" << "] ." << std::endl << std::endl;
+    }
+
+    out.close();
 }
 
 OpenInfraPlatform::Infrastructure::ExportOkstraOWL::~ExportOkstraOWL() {
