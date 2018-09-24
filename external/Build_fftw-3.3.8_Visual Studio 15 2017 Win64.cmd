6baba8cff24d022f34bfd5f20655fbf70d8afbea
Helge Hecht <helge.hecht@gmx.de>
2018-09-24 15:43:44 +0200

    Fixed external build script.

--- external/Build_fftw-3.3.8_Visual Studio 15 2017 Win64.cmd
+++ external/Build_fftw-3.3.8_Visual Studio 15 2017 Win64.cmd
@@ -8,7 +8,7 @@ wget.exe http://fftw.org/fftw-3.3.8.tar.gz -O%cd%\fftw-3.3.8.tar.gz
 7za.exe x %cd%\fftw-3.3.8.tar -o%dest%\fftw-3.3.8\src
 del %cd%\fftw-3.3.8.tar.gz
 rmdir /Q /S  %cd%\fftw-3.3.8.tar
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.1\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 C:
 cd %dest%\fftw-3.3.8\src\fftw-3.3.8
 mkdir %dest%\fftw-3.3.8\src\fftw-3.3.8\build
