ec6846285a06f85792d94bcf922de344660ee859
Helge Hecht <helge.hecht@gmx.de>
2018-10-09 09:01:31 +0200

    Made subsampling optional.

--- Infrastructure/src/OpenInfraPlatform/Infrastructure/PointCloudProcessing/PointCloud.cpp
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/PointCloudProcessing/PointCloud.cpp
@@ -1978,27 +1978,32 @@ int OpenInfraPlatform::Infrastructure::PointCloud::computeCenterlines(const buw:
 		centerpointsPointCloud->setPointScalarValue(i, chainage);
 	}
 
-	//Subsample the centerpoints cloud 10 times to include more randomnness and therefore remove patterns.
-	buw::ReferenceCounted<PointCloud> subsampled = buw::makeReferenceCounted<PointCloud>();
-	subsampled->enableScalarField();
-	int idxSS_chainage = subsampled->addScalarField("Chainage");
-	subsampled->setCurrentInScalarField(idxSS_chainage);
-	centerpointsPointCloud->setCurrentOutScalarField(idxCPC_chainage);
+	// Defaults to false.
+	bool useSubsampling = false;
 
-	int numIterations = 1000;
-	for(int i = 0; i < numIterations; i++) {
-		auto sampledPoints = buw::ReferenceCounted<CCLib::ReferenceCloud>(CCLib::CloudSamplingTools::subsampleCloudRandomly(centerpointsPointCloud.get(), (int)((1.0/numIterations) * centerpointsPointCloud->size()), nullptr));
-		int startIndex = subsampled->size();
-		subsampled->reserve(subsampled->size() + sampledPoints->size());
+	if(useSubsampling) {
+		//Subsample the centerpoints cloud 10 times to include more randomnness and therefore remove patterns.
+		buw::ReferenceCounted<PointCloud> subsampled = buw::makeReferenceCounted<PointCloud>();
+		subsampled->enableScalarField();
+		int idxSS_chainage = subsampled->addScalarField("Chainage");
+		subsampled->setCurrentInScalarField(idxSS_chainage);
+		centerpointsPointCloud->setCurrentOutScalarField(idxCPC_chainage);
 
-		for(int idx = 0; idx < sampledPoints->size(); idx++) {			
-			subsampled->addPoint(*sampledPoints->getPoint(idx));
-			subsampled->setPointScalarValue(startIndex + idx, sampledPoints->getPointScalarValue(idx));
+		int numIterations = 1000;
+		for(int i = 0; i < numIterations; i++) {
+			auto sampledPoints = buw::ReferenceCounted<CCLib::ReferenceCloud>(CCLib::CloudSamplingTools::subsampleCloudRandomly(centerpointsPointCloud.get(), (int)((1.0 / numIterations) * centerpointsPointCloud->size()), nullptr));
+			int startIndex = subsampled->size();
+			subsampled->reserve(subsampled->size() + sampledPoints->size());
+
+			for(int idx = 0; idx < sampledPoints->size(); idx++) {
+				subsampled->addPoint(*sampledPoints->getPoint(idx));
+				subsampled->setPointScalarValue(startIndex + idx, sampledPoints->getPointScalarValue(idx));
+			}
 		}
+
+		centerpointsPointCloud.swap(subsampled);
+		idxCPC_chainage = idxSS_chainage;
 	}
-	
-	centerpointsPointCloud.swap(subsampled);
-	idxCPC_chainage = idxSS_chainage;
 
 	centerpointsPointCloud->getDGMOctree()->build(callback.get());
 	centerpointsPointCloud->computeIndices();
