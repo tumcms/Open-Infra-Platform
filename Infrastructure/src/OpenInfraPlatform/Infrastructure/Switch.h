11a1ce76037b30127573202afd5bb8626ce51869
Max Werner <max.werner@tum.de>
2018-11-28 15:25:23 +0100

    Max Werner

--- Infrastructure/src/OpenInfraPlatform/Infrastructure/Switch.h
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/Switch.h
@@ -1,4 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include <string>
+
+
 class Switch {
+	int radius;
+	float SwitchAngle;
 public:
-	Switch();
+	enum switchtyp {singleSlip, doubleSlip, insideSlip, outsideSlip, crossover, scissorsCrossover} ST;
+	enum turnoutDirection {left, right} TD;
+	enum switchPoint {}SP;
+	enum tieType {wooden, concrete, steel, stoneBlock, plastics}TT;
+	int set_radius(int a);
+	int get_radius();
+	float set_SwitchAngle(float a);
+	float get_SwitchAngle();
+
 };
