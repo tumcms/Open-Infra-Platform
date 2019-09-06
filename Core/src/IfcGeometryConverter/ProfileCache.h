ffd1895e0316f9c64225efabf1b9d238715c899f
Helge Hecht <helge.hecht@gmx.de>
2019-09-06 17:05:55 +0200

    Reincluded IFC4 in list of supported formats.

--- Core/src/IfcGeometryConverter/ProfileCache.h
+++ Core/src/IfcGeometryConverter/ProfileCache.h
@@ -53,16 +53,21 @@ namespace OpenInfraPlatform {
 						std::shared_ptr<typename IfcEntityTypesT::IfcProfileDef>& ifcProfile)
 					{
 						const int profile_id = ifcProfile->getId();
+#ifdef _DEBUG
 						BLUE_LOG(trace) << "Getting ProfileConverterT for IfcProfileDef #" << profile_id;
-
+#endif
 						typename std::map<int, std::shared_ptr<ProfileConverterT<IfcEntityTypesT, IfcUnitConverterT>>>::iterator
 							it_profile_cache = profileCache.find(profile_id);
 						if(it_profile_cache != profileCache.end()) {
+#ifdef _DEBUG
 							BLUE_LOG(trace) << "Found ProfileConverterT for IfcProfileDef #" << profile_id;
+#endif
 							return it_profile_cache->second;
 						}
 						else {
+#ifdef _DEBUG
 							BLUE_LOG(trace) << "Creating ProfileConverterT for IfcProfile #" << profile_id;
+#endif
 							std::shared_ptr<ProfileConverterT<IfcEntityTypesT, IfcUnitConverterT>> profile_converter =
 								std::make_shared<ProfileConverterT<IfcEntityTypesT, IfcUnitConverterT>>(geomSettings, unitConverter);
 
@@ -70,7 +75,9 @@ namespace OpenInfraPlatform {
 
 							profileCache[profile_id] = profile_converter;
 
+#ifdef _DEBUG
 							BLUE_LOG(trace) << "Created ProfileConverterT for IfcProfile #" << profile_id;
+#endif
 							return profile_converter;
 						}
 					}
