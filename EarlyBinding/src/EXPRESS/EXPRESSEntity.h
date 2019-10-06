82f2f627bd692a3d7206a2f4e7136dc55f9ceb0e
Stefan Jaud <fcpjanc@gmail.com>
2019-10-06 22:42:25 +0200

    Added getErrorLog() utility function to express entities for easier error handling in code.

--- EarlyBinding/src/EXPRESS/EXPRESSEntity.h
+++ EarlyBinding/src/EXPRESS/EXPRESSEntity.h
@@ -46,6 +46,10 @@ public:
 		return "#" + std::to_string(m_id);
 	}
 
+	const std::string getErrorLog() const {
+		return classname() + ": (" + getStepParameter() + ")";
+	}
+
 	virtual const std::string classname() const = 0;
 
 	virtual const std::string getStepLine() const = 0;
