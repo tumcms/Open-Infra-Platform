847b296bea1cb7b05ce4f6dbb6ceaae4a65a41de
hechth <helge.hecht@gmx.de>
2020-04-15 10:29:18 +0200

    Continued on comment unit test

---
+++ UnitTests/Schemas/IFC4X1/Comments/src/Comments.cpp
@@ -0,0 +1,44 @@
+/*
+    Copyright (c) 2020 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <reader/IFC4X1Reader.h>
+#include <namespace.h>
+
+using namespace testing;
+
+class CommentsTest : public Test {
+protected:
+    virtual void SetUp() override {
+        express_model = OpenInfraPlatform::IFC4X1::IFC4X1Reader::FromFile(filename);
+    }
+
+    virtual void TearDown() override {
+        express_model.reset();
+    }
+
+
+    const std::string filename = "Data/csg-primitive-commented.ifc";
+    std::shared_ptr<oip::EXPRESSModel> express_model = nullptr;
+
+};
+
+TEST_F(CommentsTest, AllEntitiesAreRead) {
+    EXPECT_THAT(express_model->entities.size(), Eq(31));
+}
---
+++ UnitTests/Schemas/IFC4X1/namespace.h
@@ -0,0 +1,38 @@
+/*
+    Copyright (c) 2020 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef F7302D7B_948F_445E_8DB9_C722295B8E2E
+#define F7302D7B_948F_445E_8DB9_C722295B8E2E
+
+#if !defined OIP_NAMESPACE_UNITTESTS_SCHEMA_IFCX41_BEGIN
+#define OIP_NAMESPACE_UNITTESTS_SCHEMA_IFCX41_BEGIN \
+namespace OpenInfraPlatform {\
+namespace UnitTests {\
+namespace Schema {\
+namespace IFC4X1 {
+#endif
+
+#if !defined OIP_NAMESPACE_UNITTESTS_SCHEMA_IFCX41_END
+#define OIP_NAMESPACE_UNITTESTS_SCHEMA_IFCX41_END \
+}\
+}\
+}\
+}
+
+#endif
+#endif /* F7302D7B_948F_445E_8DB9_C722295B8E2E */
