3d4277e3f97719e441f6e6b2e8be18ed94b57a20
hechth <helge.hecht@gmx.de>
2020-04-20 07:35:47 +0200

    Added licensing information

--- Core/src/IfcGeometryConverter/PlacementConverterImpl.h
+++ Core/src/IfcGeometryConverter/PlacementConverterImpl.h
@@ -1,3 +1,21 @@
+/*
+    Copyright (c) 2020 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #pragma once
 #ifndef A9B1C3A9_3605_4817_803A_F41F81E53C51
 #define A9B1C3A9_3605_4817_803A_F41F81E53C51
