922c430b99fb221d5641e787dbb3b33eee87e819
hechth <helge.hecht@gmx.de>
2020-04-20 08:54:11 +0200

    Added namespace.h to IfcGeometryConverter folder and files

--- Core/src/IfcGeometryConverter/ConverterBuw.h
+++ Core/src/IfcGeometryConverter/ConverterBuw.h
@@ -30,6 +30,8 @@
 #include "CarveHeaders.h"
 #include "GeometryInputData.h"
 
+#include "namespace.h"
+
 /***********************************************************************************************/
 
 typedef buw::VertexPosition3Color3Normal3 VertexLayout;
@@ -494,17 +496,17 @@ namespace OpenInfraPlatform
 							return buw::Vector3f(0.6f, 0.6f, 0.4f);//, 1.0f);
 						}
 
-						// Geländer
+						// Gelï¿½nder
 						else if(std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcRailing>(product)) {
 							return buw::Vector3f(0.7f, 0.7f, 0.2f);//, 1.0f);
 						}
 
-						// Geländer
+						// Gelï¿½nder
 						else if(std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcPile>(product)) {
 							return buw::Vector3f(0.15f, 0.7f, 0.0f);//, 1.0f);
 						}
 
-						// Möbel
+						// Mï¿½bel
 						else if(std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcFurnishingElement>(product)) {
 							return buw::Vector3f(0.8f, 0.6f, 0.2f);//, 1.0f);
 						}
@@ -517,7 +519,7 @@ namespace OpenInfraPlatform
 							|| std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcDistributionFlowElement>(product)) {
 							return buw::Vector3f(0.4f, 0.4f, 0.6f);//, 1.0f);
 						}
-						// Platte als Dach oder Geländer?
+						// Platte als Dach oder Gelï¿½nder?
 						else if(std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcSlab>(product)) {
 							const std::shared_ptr<typename IfcEntityTypesT::IfcSlab>& slab =
 								std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcSlab>(product);
@@ -559,5 +561,7 @@ namespace OpenInfraPlatform
 		}
 	}
 }
+
+EMBED_CORE_IFCGEOMETRYCONVERTER_INTO_OIP_NAMESPACE(IfcGeometryModel)
 
 #endif
---
+++ Core/src/IfcGeometryConverter/namespace.h
@@ -0,0 +1,39 @@
+/*
+    Copyright (c) 2020 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef OpenInfraPlatform_Core_namespace_6e3a742e_afd0_4dd0_82a8_94ac0e7506b2_h
+#define OpenInfraPlatform_Core_namespace_6e3a742e_afd0_4dd0_82a8_94ac0e7506b2_h
+#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_IFCGEOMETRYCONVERTER_BEGIN
+#define OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_IFCGEOMETRYCONVERTER_BEGIN \
+namespace OpenInfraPlatform { \
+namespace Core { \
+namespace IfcGeometryConverter {
+#endif
+#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_IFCGEOMETRYCONVERTER_END
+#define OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_IFCGEOMETRYCONVERTER_END \
+}\
+}\
+}
+#endif
+#if !defined EMBED_CORE_IFCGEOMETRYCONVERTER_INTO_OIP_NAMESPACE
+#define EMBED_CORE_IFCGEOMETRYCONVERTER_INTO_OIP_NAMESPACE(X) \
+namespace oip { \
+using OpenInfraPlatform::Core::IfcGeometryConverter::X; \
+}
+#endif
+#endif // end define OpenInfraPlatform_Core_namespace_6e3a742e_afd0_4dd0_82a8_94ac0e7506b2_h
