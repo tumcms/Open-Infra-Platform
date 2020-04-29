3fa5b748ae0234bf28bf3b0f42f428f8fb31d14c
ccoetzee <cara.coetzee@tum.de>
2020-04-29 21:43:32 +0200

    Added USE_STAMP_FILE option to doxygen_add_docs() target.

--- cmake/ConfigureDoxygen.cmake
+++ cmake/ConfigureDoxygen.cmake
@@ -83,11 +83,13 @@ endforeach()
 doxygen_add_docs(OpenInfraPlatform.GenerateDocumentation ALL
 ${DOXYGEN_INPUT} 	# Source code that is (to be) documented located here
 WORKING DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}	# Current root. Change if relative base point should be different.
+USE_STAMP_FILE
 COMMENT "Generating doxymentation for TUM Open Infra Platform project.")
 else()
 doxygen_add_docs(OpenInfraPlatform.GenerateDocumentation ALL
 ${CMAKE_CURRENT_SOURCE_DIR} 	# Source code that is (to be) documented located here
 WORKING DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}	# Current root. Change if relative base point should be different.
+USE_STAMP_FILE
 COMMENT "Generating doxymentation for TUM Open Infra Platform project.")
 endif(DOXYGEN_OPTIONAL_INCLUDE_EARLYBINDING)
 
