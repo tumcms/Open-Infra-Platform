3856f3c006c445efa5040ee8ce693858ca2a082d
Stefan Jaud <ga24yuk@mytum.de>
2020-05-13 07:38:46 +0200

    Forgot one stringstream.

--- Core/src/IfcGeometryConverter/IfcImporterImpl.h
+++ Core/src/IfcGeometryConverter/IfcImporterImpl.h
@@ -112,7 +112,7 @@ static void OpenInfraPlatform::Core::IfcGeometryConverter::IfcImporterUtil::conv
 		std::shared_ptr<ItemData> itemData(new ItemData());
 		productShape->vec_item_data.push_back(itemData);
 		std::shared_ptr<typename IfcEntityTypesT::IfcGeometricRepresentationItem> axis = std::dynamic_pointer_cast<typename IfcEntityTypesT::IfcGeometricRepresentationItem>(alignment->Axis.lock());
-		repConverter->convertIfcGeometricRepresentationItem(alignment->Axis.as<typename IfcEntityTypesT::IfcGeometricRepresentationItem>(), carve::math::Matrix::IDENT(), itemData, strerr);
+		repConverter->convertIfcGeometricRepresentationItem(alignment->Axis.as<typename IfcEntityTypesT::IfcGeometricRepresentationItem>(), carve::math::Matrix::IDENT(), itemData);
 	}
 
 #ifdef _DEBUG
