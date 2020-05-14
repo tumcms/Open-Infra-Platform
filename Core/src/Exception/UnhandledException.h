134ae9bba65a5e90d861aac951112706f3e1469b
hechth <helge.hecht@gmx.de>
2020-05-14 10:41:31 +0200

    Moved UnhandledException.h implementation to cpp file

--- Core/src/Exception/UnhandledException.h
+++ Core/src/Exception/UnhandledException.h
@@ -1,20 +1,19 @@
-// Copied and modified code from "IfcPlusPlus".
-// This code is available under the OSGPL license. Original copyright notice:
+/*
+Copyright (c) 2020 Technical University of Munich
+Chair of Computational Modeling and Simulation.
 
-/* -*-c++-*- IfcPlusPlus - www.ifcplusplus.com  - Copyright (C) 2011 Fabian Gerold
- *
- * This library is open source and may be redistributed and/or modified under  
- * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
- * (at your option) any later version.  The full license is in LICENSE file
- * included with this distribution, and on the openscenegraph.org website.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * OpenSceneGraph Public License for more details.
+TUM Open Infra Platform is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License Version 3
+as published by the Free Software Foundation.
+
+TUM Open Infra Platform is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-
-// More details can be found in README.md file in the root directory.
 
 #pragma once
 #ifndef UNHANDLED_REPRESENTATION_EXCEPTION_H
@@ -31,53 +30,41 @@
 OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_EXCEPTION_BEGIN
 
 /*!
-	\brief Gets thrown if an IFC entity ot type is not supported by OIP.
+\brief Gets thrown if an IFC entity ot type is not supported by OIP.
 */
-class UnhandledException : public std::exception
-{
+class UnhandledException : public std::exception {
 public:
-	//! Constructor
-	UnhandledException() noexcept
-	{
-	}
-	//! Copy Constructor
-	UnhandledException(const UnhandledException &other) noexcept
-		: item_(other.item_)
-	{
-	}
-	/*!
-	\brief Constructor with the unhandled entity.				
-	\param[in] item The entity that is not handled.
-	*/
-	UnhandledException(const std::shared_ptr<oip::EXPRESSObject> &item) noexcept
-	{
-		item_ = item;
-	}
-	/*!
-				\brief Constructor with the unhandled entity.
-				\param[in] item The entity that is not handled.
-				*/
-	template <typename T>
-	UnhandledException(const oip::EXPRESSReference<typename T> &item) noexcept
-	{
-		item_ = item.lock();
-	}
-	//! Destructor
-	~UnhandledException() throw()
-	{
-	}
+    //! Constructor
+    UnhandledException() noexcept;
+    //! Copy Constructor
+    UnhandledException(const UnhandledException& other) noexcept;
 
-	/*!
-				\brief Returns what is not supported.
-				\returns A message with the entity name.
-				*/
-	const char *what() const throw()
-	{
-		std::string s = item_->getErrorLog() + ": IFC Entity not supported.";
-		return s.c_str();
-	}
+    /*!
+    \brief Constructor with the unhandled entity.
+    \param[in] item The entity that is not handled.
+    */
+    UnhandledException(const std::shared_ptr<oip::EXPRESSObject>& item) noexcept;
 
-	std::shared_ptr<oip::EXPRESSObject> item_;
+    /*!
+    \brief Constructor with the unhandled entity.
+    \param[in] item The entity that is not handled.
+    */
+    template <typename T>
+    UnhandledException(const oip::EXPRESSReference<typename T>& item) noexcept
+    {
+        item_ = item.lock();
+    }
+
+    //! Destructor
+    ~UnhandledException() noexcept;
+
+    /*!
+    \brief Returns what is not supported.
+    \returns A message with the entity name.
+    */
+    const char* what() const noexcept override;
+
+    std::shared_ptr<oip::EXPRESSObject> item_;
 };
 
 OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_EXCEPTION_END
