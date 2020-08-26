76b35fc9c1ffd22df63cbcc6f2b89be3e6497c30
Samuils Rulovs <62873079+SamuilsRulovs@users.noreply.github.com>
2020-08-26 14:08:55 +0200

    Added 4 new functions PlaneSurfaceViews, VertexViews, GivenNewImage_AreEqual_With_PlaneSurfaceViews and GivenNewImage_AreEqual_With_VertexViews

--- UnitTests/Schemas/IFC4X1/extruded-solid/src/extruded-solid.cpp
+++ UnitTests/Schemas/IFC4X1/extruded-solid/src/extruded-solid.cpp
@@ -83,27 +83,202 @@ TEST_F(ExtrudedSolidTest, ImageIsSaved)
 	EXPECT_NO_THROW(buw::loadImage4b(testPath("extruded-solid.png").string()));
 }
 
-{
-
-
-}
-
+TEST_F(ExtrudedSolidTest, PlaneSurfaceViews)
 {
 	// Arrange
+	const auto expected_front = buw::loadImage4b(dataPath("extruded-solid_front.png").string());
+	const auto expected_top = buw::loadImage4b(dataPath("extruded-solid_top.png").string());
+	const auto expected_bottom = buw::loadImage4b(dataPath("extruded-solid_bottom.png").string());
+	const auto expected_left = buw::loadImage4b(dataPath("extruded-solid_left.png").string());
+	const auto expected_right = buw::loadImage4b(dataPath("extruded-solid_right.png").string());
+	const auto expected_back = buw::loadImage4b(dataPath("extruded-solid_back.png").string());
 
-
-	// Assert
-}
-
-{
-	// Arrange
+	// Act (Front)
+	renderer->setViewDirection(buw::eViewDirection::Front);
+	buw::Image4b image_front = CaptureImage();
+	// Act (Top)
+	renderer->setViewDirection(buw::eViewDirection::Top);
+	buw::Image4b image_top = CaptureImage();
+	// Act (Bottom)
 	renderer->setViewDirection(buw::eViewDirection::Bottom);
+	buw::Image4b image_bottom = CaptureImage();
+	// Act (Left)
+	renderer->setViewDirection(buw::eViewDirection::Left);
+	buw::Image4b image_left = CaptureImage();
+	// Act (Right)
+	renderer->setViewDirection(buw::eViewDirection::Right);
+	buw::Image4b image_right = CaptureImage();
+	// Act (Back)
+	renderer->setViewDirection(buw::eViewDirection::Back);
+	buw::Image4b image_back = CaptureImage();
+
+	// uncomment following lines to also save the screen shot
+
+	buw::storeImage(testPath("extruded-solid_front.png").string(), image_front);
+	buw::storeImage(testPath("extruded-solid_top.png").string(), image_top);
+	buw::storeImage(testPath("extruded-solid_bottom.png").string(), image_bottom);
+	buw::storeImage(testPath("extruded-solid_left.png").string(), image_left);
+	buw::storeImage(testPath("extruded-solid_right.png").string(), image_right);
+	buw::storeImage(testPath("extruded-solid_back.png").string(), image_back);
+
 
 	// Assert
+	EXPECT_EQ(image_front, expected_front);
+	EXPECT_EQ(image_top, expected_top);
+	EXPECT_EQ(image_bottom, expected_bottom);
+	EXPECT_EQ(image_left, expected_left);
+	EXPECT_EQ(image_right, expected_right);
+	EXPECT_EQ(image_back, expected_back);
+}
 
+TEST_F(ExtrudedSolidTest, VertexViews)
+{
+	// Arrange
+	const auto expected_front_left_bottom = buw::loadImage4b(dataPath("extruded-solid_front_left_bottom.png").string());
+	const auto expected_front_right_bottom = buw::loadImage4b(dataPath("extruded-solid_front_right_bottom.png").string());
+	const auto expected_top_left_front = buw::loadImage4b(dataPath("extruded-solid_top_left_front.png").string());
+	const auto expected_top_front_right = buw::loadImage4b(dataPath("extruded-solid_top_front_right.png").string());
+	const auto expected_top_left_back = buw::loadImage4b(dataPath("extruded-solid_top_left_back.png").string());
+	const auto expected_top_right_back = buw::loadImage4b(dataPath("extruded-solid_top_right_back.png").string());
+	const auto expected_back_left_bottom = buw::loadImage4b(dataPath("extruded-solid_back_left_bottom.png").string());
+	const auto expected_right_bottom_back = buw::loadImage4b(dataPath("extruded-solid_right_bottom_back.png").string());
+
+	// Act (FrontLeftBottom)
+	renderer->setViewDirection(buw::eViewDirection::FrontLeftBottom);
+	buw::Image4b image_front_left_bottom = CaptureImage();
+	// Act (FrontRightBottom)
+	renderer->setViewDirection(buw::eViewDirection::FrontRightBottom);
+	buw::Image4b image_front_right_bottom = CaptureImage();
+	// Act (TopLeftFront)
+	renderer->setViewDirection(buw::eViewDirection::TopLeftFront);
+	buw::Image4b image_top_left_front = CaptureImage();
+	// Act (TopFrontRight)
+	renderer->setViewDirection(buw::eViewDirection::TopFrontRight);
+	buw::Image4b image_top_front_right = CaptureImage();
+	// Act (TopLeftBack)
+	renderer->setViewDirection(buw::eViewDirection::TopLeftBack);
+	buw::Image4b image_top_left_back = CaptureImage();
+	// Act (TopRightBack)
+	renderer->setViewDirection(buw::eViewDirection::TopRightBack);
+	buw::Image4b image_top_right_back = CaptureImage();
+	// Act (BackLeftBottom)
+	renderer->setViewDirection(buw::eViewDirection::BackLeftBottom);
+	buw::Image4b image_back_left_bottom = CaptureImage();
+	// Act (RightBottomBack)
+	renderer->setViewDirection(buw::eViewDirection::RightBottomBack);
+	buw::Image4b image_right_bottom_back = CaptureImage();
+
+	// uncomment following lines to also save the screen shot
+
+	buw::storeImage(testPath("extruded-solid_front_left_bottom.png").string(), image_front_left_bottom);
+	buw::storeImage(testPath("extruded-solid_front_right_bottom.png").string(), image_front_right_bottom);
+	buw::storeImage(testPath("extruded-solid_top_left_front.png").string(), image_top_left_front);
+	buw::storeImage(testPath("extruded-solid_top_front_right.png").string(), image_top_front_right);
+	buw::storeImage(testPath("extruded-solid_top_left_back.png").string(), image_top_left_back);
+	buw::storeImage(testPath("extruded-solid_top_right_back.png").string(), image_top_right_back);
+	buw::storeImage(testPath("extruded-solid_back_left_bottom.png").string(), image_back_left_bottom);
+	buw::storeImage(testPath("extruded-solid_right_bottom_back.png").string(), image_right_bottom_back);
+
+
+	// Assert
+	EXPECT_EQ(image_front_left_bottom, expected_front_left_bottom);
+	EXPECT_EQ(image_front_right_bottom, expected_front_right_bottom);
+	EXPECT_EQ(image_top_left_front, expected_top_left_front);
+	EXPECT_EQ(image_top_front_right, expected_top_front_right);
+	EXPECT_EQ(image_top_left_back, expected_top_left_back);
+	EXPECT_EQ(image_top_right_back, expected_top_right_back);
+	EXPECT_EQ(image_back_left_bottom, expected_back_left_bottom);
+	EXPECT_EQ(image_right_bottom_back, expected_right_bottom_back);
+}
+
+TEST_F(ExtrudedSolidTest, GivenNewImage_AreEqual_With_PlaneSurfaceViews)
+{
+	// Arrange
+	const auto expected_front = buw::loadImage4b(dataPath("extruded-solid_front.png").string());
+	const auto expected_top = buw::loadImage4b(dataPath("extruded-solid_top.png").string());
+	const auto expected_bottom = buw::loadImage4b(dataPath("extruded-solid_bottom.png").string());
+	const auto expected_left = buw::loadImage4b(dataPath("extruded-solid_left.png").string());
+	const auto expected_right = buw::loadImage4b(dataPath("extruded-solid_right.png").string());
+	const auto expected_back = buw::loadImage4b(dataPath("extruded-solid_back.png").string());
+
+	// Act (Front)
+	renderer->setViewDirection(buw::eViewDirection::Front);
+	buw::Image4b image_front = CaptureImage();
+	// Act (Top)
+	renderer->setViewDirection(buw::eViewDirection::Top);
+	buw::Image4b image_top = CaptureImage();
+	// Act (Bottom)
+	renderer->setViewDirection(buw::eViewDirection::Bottom);
+	buw::Image4b image_bottom = CaptureImage();
+	// Act (Left)
+	renderer->setViewDirection(buw::eViewDirection::Left);
+	buw::Image4b image_left = CaptureImage();
+	// Act (Right)
+	renderer->setViewDirection(buw::eViewDirection::Right);
+	buw::Image4b image_right = CaptureImage();
+	// Act (Back)
+	renderer->setViewDirection(buw::eViewDirection::Back);
+	buw::Image4b image_back = CaptureImage();
+
+	// Assert
+	EXPECT_EQ(image_front, expected_front);
+	EXPECT_EQ(image_top, expected_top);
+	EXPECT_EQ(image_bottom, expected_bottom);
+	EXPECT_EQ(image_left, expected_left);
+	EXPECT_EQ(image_right, expected_right);
+	EXPECT_EQ(image_back, expected_back);
+
+	// Annihilate
 
 }
 
+TEST_F(ExtrudedSolidTest, GivenNewImage_AreEqual_With_VertexViews)
 {
+	// Arrange
+	const auto expected_front_left_bottom = buw::loadImage4b(dataPath("extruded-solid_front_left_bottom.png").string());
+	const auto expected_front_right_bottom = buw::loadImage4b(dataPath("extruded-solid_front_right_bottom.png").string());
+	const auto expected_top_left_front = buw::loadImage4b(dataPath("extruded-solid_top_left_front.png").string());
+	const auto expected_top_front_right = buw::loadImage4b(dataPath("extruded-solid_top_front_right.png").string());
+	const auto expected_top_left_back = buw::loadImage4b(dataPath("extruded-solid_top_left_back.png").string());
+	const auto expected_top_right_back = buw::loadImage4b(dataPath("extruded-solid_top_right_back.png").string());
+	const auto expected_back_left_bottom = buw::loadImage4b(dataPath("extruded-solid_back_left_bottom.png").string());
+	const auto expected_right_bottom_back = buw::loadImage4b(dataPath("extruded-solid_right_bottom_back.png").string());
 
+	// Act (FrontLeftBottom)
+	renderer->setViewDirection(buw::eViewDirection::FrontLeftBottom);
+	buw::Image4b image_front_left_bottom = CaptureImage();
+	// Act (FrontRightBottom)
+	renderer->setViewDirection(buw::eViewDirection::FrontRightBottom);
+	buw::Image4b image_front_right_bottom = CaptureImage();
+	// Act (TopLeftFront)
+	renderer->setViewDirection(buw::eViewDirection::TopLeftFront);
+	buw::Image4b image_top_left_front = CaptureImage();
+	// Act (TopFrontRight)
+	renderer->setViewDirection(buw::eViewDirection::TopFrontRight);
+	buw::Image4b image_top_front_right = CaptureImage();
+	// Act (TopLeftBack)
+	renderer->setViewDirection(buw::eViewDirection::TopLeftBack);
+	buw::Image4b image_top_left_back = CaptureImage();
+	// Act (TopRightBack)
+	renderer->setViewDirection(buw::eViewDirection::TopRightBack);
+	buw::Image4b image_top_right_back = CaptureImage();
+	// Act (BackLeftBottom)
+	renderer->setViewDirection(buw::eViewDirection::BackLeftBottom);
+	buw::Image4b image_back_left_bottom = CaptureImage();
+	// Act (RightBottomBack)
+	renderer->setViewDirection(buw::eViewDirection::RightBottomBack);
+	buw::Image4b image_right_bottom_back = CaptureImage();
 
+	// Assert
+	EXPECT_EQ(image_front_left_bottom, expected_front_left_bottom);
+	EXPECT_EQ(image_front_right_bottom, expected_front_right_bottom);
+	EXPECT_EQ(image_top_left_front, expected_top_left_front);
+	EXPECT_EQ(image_top_front_right, expected_top_front_right);
+	EXPECT_EQ(image_top_left_back, expected_top_left_back);
+	EXPECT_EQ(image_top_right_back, expected_top_right_back);
+	EXPECT_EQ(image_back_left_bottom, expected_back_left_bottom);
+	EXPECT_EQ(image_right_bottom_back, expected_right_bottom_back);
+
+	// Annihilate
+
+}
