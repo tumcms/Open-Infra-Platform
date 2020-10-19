2143c0d4b4132e42b8ea6f5e19984981b410fdc5
Mauplatteau <mauricio.platteau@tum.de>
2020-10-19 13:23:56 +0200

    UnitTest with SSH on a line segment are working

--- UnitTests/Schemas/IFC4X3_RC1/SSH-Rectangle_var/src/SSH-Rectangle_Var.cpp
+++ UnitTests/Schemas/IFC4X3_RC1/SSH-Rectangle_var/src/SSH-Rectangle_Var.cpp
@@ -115,14 +115,14 @@ TEST_F(SSHRectangleVarTest, PlaneSurfaceViews)
 	buw::Image4b image_back = CaptureImage();
 
 	// uncomment following lines to also save the screen shot
-	
+	/*
 	buw::storeImage(testPath("SSH-Rectangle_Var_front.png").string(), image_front);
 	buw::storeImage(testPath("SSH-Rectangle_Var_top.png").string(), image_top);
 	buw::storeImage(testPath("SSH-Rectangle_Var_bottom.png").string(), image_bottom);
 	buw::storeImage(testPath("SSH-Rectangle_Var_left.png").string(), image_left);
 	buw::storeImage(testPath("SSH-Rectangle_Var_right.png").string(), image_right);
 	buw::storeImage(testPath("SSH-Rectangle_Var_back.png").string(), image_back);
-	
+	*/
 
 	// Assert
 	EXPECT_EQ(image_front, expected_front);
@@ -171,7 +171,7 @@ TEST_F(SSHRectangleVarTest, VertexViews)
 	buw::Image4b image_right_bottom_back = CaptureImage();
 
 	// uncomment following lines to also save the screen shot
-	
+	/*
 	buw::storeImage(testPath("SSH-Rectangle_Var_front_left_bottom.png").string(), image_front_left_bottom);
 	buw::storeImage(testPath("SSH-Rectangle_Var_front_right_bottom.png").string(), image_front_right_bottom);
 	buw::storeImage(testPath("SSH-Rectangle_Var_top_left_front.png").string(), image_top_left_front);
@@ -180,7 +180,7 @@ TEST_F(SSHRectangleVarTest, VertexViews)
 	buw::storeImage(testPath("SSH-Rectangle_Var_top_right_back.png").string(), image_top_right_back);
 	buw::storeImage(testPath("SSH-Rectangle_Var_back_left_bottom.png").string(), image_back_left_bottom);
 	buw::storeImage(testPath("SSH-Rectangle_Var_right_bottom_back.png").string(), image_right_bottom_back);
-	
+	*/
 
 	// Assert
 	EXPECT_EQ(image_front_left_bottom, expected_front_left_bottom);
