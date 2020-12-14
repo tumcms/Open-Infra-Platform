e3aca55cc4d2840b5faf4ceada6ffff6391048dc
Stefan Jaud <ga24yuk@mytum.de>
2020-12-14 12:54:04 +0100

    Refactored the output procedure to write to an UTF-8 file

--- Tools/namespaceGenerator.py
+++ Tools/namespaceGenerator.py
@@ -17,6 +17,7 @@ import uuid
 import string
 import sys
 import argparse
+import io
 
 from optparse import OptionParser
 
@@ -43,6 +44,9 @@ textHeader = u"""/*
 */
 """
 
+def writeLine(file, str):
+    file.write(str + "\n")
+
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("moduleName")
@@ -52,30 +56,33 @@ def main(argv):
     moduleName = args.moduleName
     fileName = args.fileName
     
-	strUuid = str(uuid.uuid4())
-	strUuid = strUuid.replace("-","_")
-	
-	print(textHeader)	
-	print("#pragma once")
-	print("#ifndef OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")
-	print("#define OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")    
-	print("#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_BEGIN")
-	print("#define OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_BEGIN \\")
-	print("namespace OpenInfraPlatform { \\")
-	print("namespace " + moduleName + " {")
-	print("#endif")
-	print("#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_END")
-	print("#define OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_END \\")
-	print("}\\")
-	print("}")
-	print("#endif")
-	print("#if !defined EMBED_INTO_OIP_NAMESPACE")
-	print("#define EMBED_INTO_OIP_NAMESPACE(X) \\")
-	print("namespace oip { \\")
-	print("using OpenInfraPlatform::" + moduleName + "::X; \\")
-	print("}")
-	print("#endif")   
-	print("#endif // end define OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")
-	   
+    strUuid = str(uuid.uuid4())
+    strUuid = strUuid.replace("-","_")
+    
+    with io.open(fileName, 'w', encoding='utf8') as f:
+       writeLine(f, textHeader)
+       writeLine(f, u"#pragma once")
+       writeLine(f, u"#ifndef OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")
+       writeLine(f, u"#define OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")    
+       writeLine(f, u"#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_BEGIN")
+       writeLine(f, u"#define OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_BEGIN \\")
+       writeLine(f, u"namespace OpenInfraPlatform { \\")
+       writeLine(f, u"namespace " + moduleName + " {")
+       writeLine(f, u"#endif")
+       writeLine(f, u"#if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_END")
+       writeLine(f, u"#define OIP_NAMESPACE_OPENINFRAPLATFORM_" + moduleName.upper() + "_END \\")
+       writeLine(f, u"}\\")
+       writeLine(f, u"}")
+       writeLine(f, u"#endif")
+       writeLine(f, u"#if !defined EMBED_" + moduleName.upper() + "_INTO_OIP_NAMESPACE")
+       writeLine(f, u"#define EMBED_" + moduleName.upper() + "_INTO_OIP_NAMESPACE(X) \\")
+       writeLine(f, u"namespace oip { \\")
+       writeLine(f, u"using OpenInfraPlatform::" + moduleName + "::X; \\")
+       writeLine(f, u"}")
+       writeLine(f, u"#endif")   
+       writeLine(f, u"#endif // end define OpenInfraPlatform_" + moduleName + "_namespace_" + strUuid + "_h")
+       
+    print("Wrote the namespace header of " + moduleName + " to " + fileName)
+
 if __name__ == "__main__":
    main(sys.argv[1:])
