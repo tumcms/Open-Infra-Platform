888539a1acc99a1ea4c829bece5cfc9fcc74e3d1
Stefan Jaud <ga24yuk@mytum.de>
2020-12-14 13:36:20 +0100

    Replacing them with new headers

---
+++ EarlyBinding/src/namespace.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (c) 2021 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file has been generated with Tools/namespaceGenerator.py .
+*/
+
+
+#pragma once
+#ifndef OpenInfraPlatform_EarlyBinding_namespace_fd1c2fc1_3bc1_4795_98db_818c59086f65_h
+#define OpenInfraPlatform_EarlyBinding_namespace_fd1c2fc1_3bc1_4795_98db_818c59086f65_h
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_EARLYBINDING_BEGIN
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_EARLYBINDING_BEGIN \
+namespace OpenInfraPlatform { \
+namespace EarlyBinding {
+#    endif
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_EARLYBINDING_END
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_EARLYBINDING_END \
+}\
+}
+#    endif
+
+#    if !defined EMBED_EARLYBINDING_INTO_OIP_NAMESPACE
+#        define EMBED_EARLYBINDING_INTO_OIP_NAMESPACE(X) \
+namespace oip { \
+using OpenInfraPlatform::EarlyBinding::X; \
+}
+#    endif
+
+#endif // end define OpenInfraPlatform_EarlyBinding_namespace_fd1c2fc1_3bc1_4795_98db_818c59086f65_h
---
+++ ExpressBindingGenerator/src/General/namespace.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (c) 2021 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file has been generated with Tools/namespaceGenerator.py .
+*/
+
+
+#pragma once
+#ifndef OpenInfraPlatform_ExpressBindingGenerator_namespace_d9a6d48c_0c2a_4eb4_8b2b_6332b460feea_h
+#define OpenInfraPlatform_ExpressBindingGenerator_namespace_d9a6d48c_0c2a_4eb4_8b2b_6332b460feea_h
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_EXPRESSBINDINGGENERATOR_BEGIN
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_EXPRESSBINDINGGENERATOR_BEGIN \
+namespace OpenInfraPlatform { \
+namespace ExpressBindingGenerator {
+#    endif
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_EXPRESSBINDINGGENERATOR_END
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_EXPRESSBINDINGGENERATOR_END \
+}\
+}
+#    endif
+
+#    if !defined EMBED_EXPRESSBINDINGGENERATOR_INTO_OIP_NAMESPACE
+#        define EMBED_EXPRESSBINDINGGENERATOR_INTO_OIP_NAMESPACE(X) \
+namespace oip { \
+using OpenInfraPlatform::ExpressBindingGenerator::X; \
+}
+#    endif
+
+#endif // end define OpenInfraPlatform_ExpressBindingGenerator_namespace_d9a6d48c_0c2a_4eb4_8b2b_6332b460feea_h
