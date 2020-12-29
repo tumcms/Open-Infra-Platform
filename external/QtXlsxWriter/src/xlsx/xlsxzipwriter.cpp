229027d001717bbd48d8fcde87ef37b2539eefd1
hechth <helge.hecht@gmx.de>
2020-12-29 19:30:26 +0100

    Updated to new Qt

--- external/QtXlsxWriter/src/xlsx/xlsxzipwriter.cpp
+++ external/QtXlsxWriter/src/xlsx/xlsxzipwriter.cpp
@@ -59,8 +59,11 @@
 	#elif (QT_VERSION == QT_VERSION_CHECK(5,12,1))
 		#include  "QtGui/5.12.1/QtGui/private/qzipwriter_p.h"
 	#elif (QT_VERSION == QT_VERSION_CHECK(5,12,2))
-	#include  "QtGui/5.12.2/QtGui/private/qzipwriter_p.h"
+		#include  "QtGui/5.12.2/QtGui/private/qzipwriter_p.h"
+	#elif (QT_VERSION == QT_VERSION_CHECK(5,14,2))
+		#include  "QtGui/5.14.2/QtGui/private/qzipwriter_p.h"
 	#endif
+
 #endif
 
 //#include <private/qzipwriter_p.h>
