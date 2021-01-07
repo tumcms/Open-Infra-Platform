97447bf3081fbc58423b707ef2cad771a543dcf6
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2021-01-07 18:50:11 +0100

    Added missing "pragma once" in SplineInterpretation.cpp

--- Rendering/Tools/SplineInterpretation.cpp
+++ Rendering/Tools/SplineInterpretation.cpp
@@ -15,6 +15,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 #include "SplineInterpretation.h"
 using OpenInfraPlatform::UserInterface::SplineInterpretationElement;
 
