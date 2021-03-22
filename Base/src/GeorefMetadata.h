14f36ab967aaf1dab3645e2467b6ff83bcc7db20
Stefan Jaud <ga24yuk@mytum.de>
2021-03-22 14:08:56 +0100

    transformation matrix error

--- Base/src/GeorefMetadata.h
+++ Base/src/GeorefMetadata.h
@@ -64,8 +64,8 @@ public:
 	{
 		return carve::math::Matrix(
 			1.0, 0.0, 0.0, 0.0,
-			0.0, cos(angle), sin(angle), 0.0,
-			0.0, sin(angle), -cos(angle), 0.0,
+			0.0, cos(angle), -sin(angle), 0.0,
+			0.0, sin(angle), cos(angle), 0.0,
 			0.0, 0.0, 0.0, 1.0);
 	}
 
