fc9b7c380961e73f0931052b31589fde80018b14
unknown <moiezhsyed@gmail.com>
2023-03-05 23:30:54 +0100

    add OSM class namespace

---
+++ Core/src/OsmDataConverter/namespace.h
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2021 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file has been generated with Tools/namespaceGenerator.py .
+*/
+
+
+#pragma once
+#ifndef OpenInfraPlatform_Core_namespace_2e59ccad_8fb5_4dc1_9eeb_689d15b8f45b_h
+#define OpenInfraPlatform_Core_namespace_2e59ccad_8fb5_4dc1_9eeb_689d15b8f45b_h
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_OSMDATACONVERTER_BEGIN
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_OSMDATACONVERTER_BEGIN \
+namespace OpenInfraPlatform { \
+namespace Core { \
+namespace OsmDataConverter {
+#    endif
+
+#    if !defined OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_OSMDATACONVERTER_END
+#        define OIP_NAMESPACE_OPENINFRAPLATFORM_CORE_OSMDATACONVERTER_END \
+}\
+}\
+}
+#    endif
+
+#    if !defined EMBED_CORE_OSMDATACONVERTER_INTO_OIP_NAMESPACE
+#        define EMBED_CORE_OSMDATACONVERTER_INTO_OIP_NAMESPACE(X) \
+namespace oip { \
+using OpenInfraPlatform::Core::OsmDataConverter::X; \
+}
+#    endif
+
+#endif // end define OpenInfraPlatform_Core_namespace_2e59ccad_8fb5_4dc1_9eeb_689d15b8f45b_h
